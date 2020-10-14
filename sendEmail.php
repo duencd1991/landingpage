178de9fc0a90db2265bb24f55b2cf259b929a6f1
Nguyen Canh Duc Due <due.nguyencanhduc@mbs.com.vn>
2020-10-14 11:37:45 +0700

    Done landing-page

---
+++ sendEmail.php
@@ -0,0 +1,25 @@
+<?php
+$username = $_POST["username"];
+$email = $_POST["email"];
+$phonenumber = $_POST["phonenumber"];
+$to      = 'nhaphotoanphat.info@gmail.com';
+
+// Subject
+$subject = 'Get data from Landing-Page';
+
+// Message
+$message = "Họ và tên: $username; Email: $email; SĐT: $phonenumber";
+
+// To send HTML mail, the Content-type header must be set
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=UTF-8';
+
+// Additional headers
+$headers[] = 'To: Admin <nhaphotoanphat.info@gmail.com>';
+$headers[] = 'From: Get data from Landing-Page <nhaphotoanphat.info@gmail.com>';
+// $headers[] = 'Cc: birthdayarchive@example.com';
+// $headers[] = 'Bcc: birthdaycheck@example.com';
+
+// Mail it
+mail($to, $subject, $message, implode("\r\n", $headers));
+?>
